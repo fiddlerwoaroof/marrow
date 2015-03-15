422508c9689518a54e509cb166516ead3f6b66ff
fiddlerwoaroof <fiddlerwoaroof@gmail.com>
2015-03-15 19:52:07 -0400

    Cleaning up main
    - cleaned up imports
    - added local configuration file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,6 @@
-from flask import Flask, session, redirect, url_for, escape, request, abort, g
-import json
+from flask import Flask
 import os
 import base64
-import psycopg2
 
 from marrow import database
 from marrow import user
@@ -11,25 +9,21 @@ from marrow import bone
 app = Flask(__name__)
 app.teardown_appcontext(database.close_connection)
 app.config["APPLICATION_ROOT"] = "/api"
-app.secret_key = base64.b64encode(os.urandom(24))
-app.debug = True
 
+try:
+    from marrow_config import secret_key
+except ImportError:
+    secret_key = base64.b64encode(os.urandom(24))
+
+app.secret_key = secret_key
 
 # Blueprints #
 user.get_users(app)
 app.register_blueprint(user.user_blueprint, url_prefix='/user')
 app.register_blueprint(bone.bone_blueprint, url_prefix='/bones')
 
-
-links = {}
-def get_feeds(filename):
-    global links
-    with open(filename) as f:
-        links = json.load(f)
-
 @app.route('/')
-def index():
-    return 'Hello World'
+def index(): return 'Hello World'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
