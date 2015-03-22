a7edc10fe98e15c81ab10433f6d6056aadf6f31f
fiddlerwoaroof <fiddlerwoaroof@gmail.com>
2015-03-22 01:29:32 -0400

    added sample config

---
+++ marrow_config.sample.py
@@ -0,0 +1,11 @@
+import os
+import base64
+
+class config:
+    debug = True
+    secret_key = "<some random string>"
+    db = "<theDatabaseName>"
+    user = "<theDatabaseUser>"
+    password = "<theDatabasePassword>"
+    host = "localhost"
+
