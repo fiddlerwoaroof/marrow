a1a0a08741d81f1c745e627ea1e825c004ffd23d
fiddlerwoaroof <fiddlerwoaroof@gmail.com>
2015-03-22 06:16:43 -0400

    Fix Access Denied errors by changing the user-agent

--- src/marrow/bone.py
+++ src/marrow/bone.py
@@ -33,6 +33,16 @@ def clean_url(url):
         netloc, path = path, netloc
     return urlparse.urlunparse((scheme, netloc, path, params, query, fragment))
 
+handler=urllib2.HTTPSHandler()
+opener = urllib2.build_opener(handler)
+opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36')]
+urllib2.install_opener(opener)
+
+handler=urllib2.HTTPHandler()
+opener = urllib2.build_opener(handler)
+opener.addheaders = [('User-agent', 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36')]
+urllib2.install_opener(opener)
+
 def get_title(url):
     data = urllib2.urlopen(url)
     etree = lxml.html.parse(data)
